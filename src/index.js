047425988268381e35f2fd9d124895d2d8a44dff
Darya Zaloha <dashulia.zaloga@gmail.com>
2023-04-06 15:07:29 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import './css/styles.css';
+import {fetchCountries} from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from "notiflix";
 
 const DEBOUNCE_DELAY = 300;
+
+const countryInput = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
+
+countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY))
+
+function onCountryInput(event) {
+    event.preventDefault();
+    const countryName = countryInput.value.trim();
+
+    if (countryName === '') {
+        return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+    }
+
+    fetchCountries(countryName)
+    .then(countries => {
+      countryList.innerHTML = ''
+      countryInfo.innerHTML = ''
+      if (countries.length === 1) {
+        countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+        countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries))
+      } else if (countries.length >= 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+
+      } else {
+        countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+      }
+    })
+    .catch(isNotCountry)
+}
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(({ name, flags }) => {
+      return `
+          <li class="country-list__item">
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 70px height = 50px>
+              <h2 class="country-list__name">${name.official}</h2>
+          </li>
+          `
+    })
+    .join('')
+  return markup
+}
+
+function renderCountryInfo(countries) {
+  const markup = countries
+    .map(({ capital, population, languages }) => {
+      return `
+        <ul class="country-info__list">
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+        </ul>
+        `
+    })
+    .join('')
+  return markup
+}
+
+function isNotCountry() {
+    Notiflix.Notify.failure('Oops, there is no country with that name')
+}
